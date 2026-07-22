4e34bfa38e1ee91bfa02949c7ae5802db8d6cdc1
Roqaiah Anjum <you@example.com>
2026-07-22 20:57:12 +0530

    Add valid Inter woff2 font files in public/assets/fonts/ and @font-face definitions in index.css

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,19 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'prevent-font-html-fallback',
-      configureServer(server) {
-        server.middlewares.use((req, res, next) => {
-          if (req.url && req.url.match(/\.(woff2?|ttf|eot)(\?.*)?$/)) {
-            res.statusCode = 404;
-            return res.end();
-          }
-          next();
-        });
-      }
-    }
-  ]
+  plugins: [react()],
 })
